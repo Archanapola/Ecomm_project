dab1284f6f9e6440ea633152d2af8a54530a1b7b
vishnu190tech <vishnuvardhan@techarion.com>
2024-11-09 15:07:49 +0530

    static

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,9 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/' 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Collect static files here
 
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Store uploaded media here
+
 MEDIA_ROOT= BASE_DIR/'media'
 LOGIN_REDIRECT_URL = '/profile/'
 
