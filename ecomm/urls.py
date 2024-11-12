073c3af940cd27ae6e5ea13ed19dfbf46510877f
vishnu190tech <vishnuvardhan@techarion.com>
2024-11-12 22:31:20 +0530

    urls of ecomm

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     #path('index/', views.index, name = 'indexUrl'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Archana Fashions"
+admin.site.site_header = "Archana Fashions"
+admin.site.site_header = "Welcome to Archana Fashions"
